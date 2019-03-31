9bf31569d0527d8708b7dcc15fb49cb07f32cf2a
Icaro Tavares <icarotav4@gmail.com>
2019-03-31 04:05:49 -0300

    Adicionado middlewares de erro

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const {
+  notFoundMiddleware,
+  handleErrorMiddleware,
+  sendErrorMiddleware,
+} = require('./bin/error-middlewares')
+
 var app = express();
 
 app.use(logger('dev'));
@@ -10,5 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
+app.use(sendErrorMiddleware);
 
 module.exports = app;
