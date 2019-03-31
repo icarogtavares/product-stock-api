d2012daecf2cda9d18b911826a9829b962251665
Icaro Tavares <icarotav4@gmail.com>
2019-03-31 05:01:01 -0300

    Corrigidas variáveis de ambiente para configuração do sequelize

--- src/config/index.js
+++ src/config/index.js
@@ -5,9 +5,9 @@ module.exports = {
     salt: process.env.SALT || 's3cr3t_s4lt',
   },
   development: {
-    username: 'root',
-    password: null,
-    database: 'product_stock',
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'product_stock',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
