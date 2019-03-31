12f0e3de11ca66f4771f88fd55c8a50a639934a6
Icaro Tavares <icarotav4@gmail.com>
2019-03-31 03:04:49 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+module.exports = app;
