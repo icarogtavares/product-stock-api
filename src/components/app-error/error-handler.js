4e7444e4a47200ec8438925f1a75312890e8ce5e
Icaro Tavares <icarotav4@gmail.com>
2019-03-31 03:24:27 -0300

    Criado arquivo de tratamento de erro

---
+++ src/components/app-error/error-handler.js
@@ -0,0 +1,44 @@
+const status = require('http-status');
+const sequelize = require('sequelize');
+const { logger } = require('../../bin/logger');
+
+/**
+ * Retorna se um erro é esperado ou não
+ * @param {Error} error - Objeto do tipo Error
+ */
+const isTrustedError = (error) => {
+  return error.isOperational;
+}
+
+/**
+ * Trata o erro recebido
+ * @param {Error} error - Objeto do tipo Error
+ */
+const handleError = (error) => {
+  let errorHandled = error;
+
+  if (errorHandled instanceof sequelize.ValidationError) {
+    errorHandled = Object.assign({}, {
+      status: status.BAD_REQUEST,
+      isOperational: true,
+    }, error);
+  } else if (errorHandled.name === 'UnauthorizedError') {
+    errorHandled = Object.assign({}, {
+      status: status.UNAUTHORIZED,
+      isOperational: true,
+    }, error);
+  }
+
+  if (isTrustedError(errorHandled)) {
+    logger.error(errorHandled.description || errorHandled.message);
+  } else {
+    logger.error(errorHandled.stack);
+  }
+
+  return errorHandled;
+}
+
+module.exports = {
+  isTrustedError,
+  handleError,
+};
