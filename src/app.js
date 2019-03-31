16c0cbc83e54baec51462798a8ab9064c2ec449e
Icaro Tavares <icarotav4@gmail.com>
2019-03-31 03:29:04 -0300

    Movido app.js para pasta src

--- src/app.js
+++ src/app.js
@@ -9,4 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 module.exports = app;
